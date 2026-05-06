b4f6223621a273e17935a0f1483367d58e475046
Raul <raulrauta23@yahoo.ro>
2026-05-06 18:30:03 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Intro from "@/components/Intro";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
